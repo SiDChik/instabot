0d31af12bfe75815ece74f26bdbfc334d231fb84
Stanislav Degtyarev <sidchik@mail.ru>
2019-12-11 09:41:49 +0300

    update package

--- insta/lib.py
+++ insta/lib.py
@@ -402,6 +402,21 @@ class InstaLib:
         if not self.api.last_response or self.api.last_response.status_code // 100 >= 4:
             raise Response4xx()
 
+    async def get_graph_followers(self, user_id, next_max_id=None):
+        url = "https://www.instagram.com/graphql/query/?query_hash=56066f031e6239f35a904ac20c9f37d9&variables="
+        variables = {"id": str(user_id), "first": 50, "include_reel": True, "fetch_mutual": True}
+
+        if next_max_id:
+            variables['after'] = next_max_id
+
+        url = url + json.dumps(variables)
+        await self.check_interval('graph_followers')
+        logger.info(f'Fetching Followers {user_id}')
+        response = await wrap(lambda: self.api.session.get(url))()
+        data = response.json()
+
+        return data
+
     async def extract_users_reels(self):
         _M = {'users_reels': [], 'Watched': 0, 'reels': [], 'watching_reels': True, 'lock': False, 'watched_users': 0}
 
